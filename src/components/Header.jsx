89b63a943c90557163a28c9af7a2ac322add531e
Adeyemi-theRustyDev <abiadeabdulazeez@gmail.com>
2023-11-27 00:44:15 +0100

    UI coming in; icons settled (Linkedin icon not yet ready)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
 const Header = () => {
     return (
-        <header className="bg-slate-950 w-screen">
+        <header className="w-screen bg-gray-900">
             <nav className="flex justify-between px-4 py-8">
-                <h1>Abiade Abdulazeez</h1>
 
-                <ul className="flex flex-col md:flex-row ">
+                <div className="flex container justify-between cursor-pointer md:w-auto">
+                    <h1>Abiade Abdulazeez</h1>
+                    <div className="toggler md:hidden w-8 h-8 z-50">
+                        <img className="container" src="/assets/hamburger-menu.svg" alt="Menu" />
+                    </div>
+                </div>
+                
+
+                <ul className="hidden flex-col md:flex-row md:flex gap-8">
 
                     <li><a href="#">About me</a></li>
                     <li><a href="#">Projects</a></li>
