c5f9a669015a2b335f17206859cb2a64e04fdec2
Adeyemi-theRustyDev <abiadeabdulazeez@gmail.com>
2024-02-22 11:23:19 +0100

    general backup

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./Home";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <>
       <Header />
 
-      <div className="px-4 py-8">
-        <Home />
-      </div>
+      <Router>
+        <div className="px-4 py-8">
+          <Routes>
+          <Route  element={<Home />} path="/" />
+          {/* <Route  element={<Home />} path="/"/> */}
+          </Routes>
+        </div>
+      </Router>
       <Footer />
     </>
   );
